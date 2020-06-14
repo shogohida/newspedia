4b43c5ec97771a46c1b945e432bf17001f239efd
Shogo Hida <shogo.hida@gmail.com>
2020-06-15 00:09:00 +0900

    added news api to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,9 +34,51 @@ class ArticlesController < ApplicationController
           @article.save
         end
       end
-
+      # elsif @website.name == "Financial Times"
+      #   @website.datetime = DateTime.now - 1
+      #   # DateTime.now - 1 ??? daily news
+      #   # 上書くとユーザーの入力リセットされるよ
+      #   url2 = "https://api.ft.com/content/notifications?apiKey=59cbaf20e3e06d3565778e7b7b9ba4f2d4ed4887b6861c77c7eda766&since=#{@website.datetime}"
+      #   # DateTime.now or Time.nowを別のクラスに直さないといけない　ももとやったやつ
+      #   # 2020-06-12T13:50:00.000Z
+      #   ft_serialized = open(url2).read
+      #   @recent_articles = JSON.parse(ft_serialized)
+      #   @recent_articles["notifications"].each do |article|
+      #     # @array << article["lead_paragraph"]
+      #     @article = Article.new(
+      #       website: @website,
+      #       url: article["apiUrl"][0..7] + article["apiUrl"][12..-1]
+      #     )
+      #     if @article.valid?
+      #       @article.save
+      #     end
+      #   end
+    elsif @website.name == "Wikipedia"
+      @page = Wikipedia.find(@website.keyword)
+      @article = Article.new(
+        website: @website,
+        content: @page.summary
+      )
+      if @article.valid?
+        @article.save
+      end
+    elsif @website.name == "News API"
+      @website.country = "jp"
+      url3 = "https://newsapi.org/v2/top-headlines?country=#{@website.country}&apiKey=3370e2330c5145c8beababe2e2110742"
+      news_api_serialized = open(url3).read
+      @country_articles = JSON.parse(news_api_serialized)["articles"]
+      @country_articles.each do |article|
+        @article = Article.new(
+          website: @website,
+          name: article["title"],
+          content: article["description"]
+        )
+        if @article.valid?
+          @article.save
+        end
+      end
     end
-    @articles = Article.all
+    @articles = Article.where(website_id: @website.id)
   end
 
   def show
