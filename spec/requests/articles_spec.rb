c4e5e2a3dd2a1f7d279c2a03ef69a7643c46d46e
Shogo Hida <shogo.hida@gmail.com>
2020-06-18 23:01:17 +0900

    added controller test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -4,26 +4,29 @@ require 'rails_helper'
 # Wikipedia, 100% exact
 # FT, time must be before Time.now
 # for all,
-RSpec.describe Article do
-  describe '#index' do
+RSpec.describe "Articles", type: :request do
+  describe "#index" do
     # need to use before?? or let?
-    context "@article is saved" do
-
-      it "@article is saved in db" do
-        expect(article.save).to be_truthy
+    context "article is saved" do
+      it "article is saved in db" do
+        website = Website.new(name: "Yomiuri")
+        article = Article.new(content: "法務大臣逮捕", website: website)
+        expect(article).to be_valid
         # maybe this one, expect(article.save).to be_truthy
       end
 
       # これいらない気がする
       it "directs to article/index page" do
       end
-
     end
 
-    context "@article is not saved" do
-
-      it "@article is not saved in db" do
-        expect(article.save).to be_falthy
+    context "article is not saved" do
+      it "article is not saved in db" do
+        website = Website.new(name: "Yomiuri")
+        article1 = Article.create(content: "法務大臣逮捕", website: website)
+        article2 = Article.new(content: "法務大臣逮捕", website: website)
+        article2.valid?
+        expect(article2.errors[:content]).to include("has already been taken")
       end
 
       it "directs to article/index page" do
