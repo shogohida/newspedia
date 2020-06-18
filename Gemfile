59de836c4818441ee7d8092002414bac9e53ca99
Shogo Hida <shogo.hida@gmail.com>
2020-06-18 13:29:25 +0900

    created test files

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "news-api"
 
 group :development, :test do
   gem 'rspec-rails', '~> 4.0.0'
+  gem 'factory_bot_rails'
 end
 
 gem 'autoprefixer-rails'
---
+++ spec/factories/user.rb
@@ -0,0 +1,16 @@
+# FactoryBot.define do
+
+#   # Userモデルのテストデータaliceを定義
+#   factory :alice, class: User do
+#     name 'Alice'
+#     admin true
+#   end
+
+#   factory :bob, class: User do
+#     name 'Bob'
+#     admin false
+#   end
+
+#   # ...
+
+# end
