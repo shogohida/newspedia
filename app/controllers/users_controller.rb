2d9a80a31a2d43bcd3eb45c926788c974ef711f6
Shogo Hida <shogo.hida@gmail.com>
2020-07-05 23:43:18 +0900

    added logos, buttons and changed design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    @user.save
+    if @user.save
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
