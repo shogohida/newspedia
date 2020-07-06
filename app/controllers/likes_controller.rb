4daf33fa5b5bcdad73a8e15278d6025e94353818
Shogo Hida <shogo.hida@gmail.com>
2020-07-06 18:57:53 +0900

    added ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,18 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(article_id: params[:article_id])
     @like.save
+    respond_to do |format|
+      format.html
+      format.json { render json: { article: @article } }
+    end
   end
 
   def destroy
     @like = Like.find_by(article_id: params[:article_id], user_id: current_user.id)
     @like.destroy
+    respond_to do |format|
+      format.html
+      format.json { render json: { article: @article } }
+    end
   end
 end
--- app/javascript/components/like_button.js
+++ app/javascript/components/like_button.js
@@ -2,12 +2,14 @@ const initLikeBtn = () => {
   const likeBtn = document.querySelector("#like_button")
   if (likeBtn) {
     likeBtn.addEventListener("click", () => {
-      const loadingCtn = document.querySelector(".loading-container")
-      if (loadingCtn) loadingCtn.classList.add("loading")
-      likeBtn.remove()
+      // const loadingCtn = document.querySelector(".loading-container")
+      // if (loadingCtn) loadingCtn.classList.add("loading")
+      // likeBtn.remove()
+      // likeBtn.toggle()
+      // likeBtn.innerHTML = "<%=j render 'likes/like', article: @article %>";
+      // document.querySelector("#likes_number_<%= @article %>").innerHTML = "The number of likes: <%= @article.likes.count %>"
     })
   }
 }
-
 
 export { initLikeBtn }
