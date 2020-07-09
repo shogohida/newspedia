f3d7943650dad0aefd5a46f70d67af258db9c43d
Shogo Hida <shogo.hida@gmail.com>
2020-07-09 17:27:41 +0900

    added more tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,31 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
+  let(:article) { FactoryBot.build(:article) }
   describe "#index" do
     # need to use before?? or let?
     context "article is saved" do
       it "is saved in db" do
-        # website = Website.new(name: "Yomiuri")
-        # article = Article.new(content: "法務大臣逮捕", website: website)
-        article = FactoryBot.build(:article)
         expect(article).to be_valid
         # maybe this one, expect(article.save).to be_truthy
       end
 
-      # これいらない気がする
       it "directs to article/index page" do
       end
     end
 
     context "article is not saved" do
       it "is not saved in db" do
-        # website = Website.new(name: "Yomiuri")
-        # article1 = Article.create(content: "法務大臣逮捕", website: website)
-        # article2 = Article.new(content: "法務大臣逮捕", website: website)
         article1 = FactoryBot.create(:article)
-        article2 = FactoryBot.build(:article)
-        article2.valid?
-        expect(article2.errors[:content]).to include("has already been taken")
+        # article = FactoryBot.build(:article)
+        article.valid?
+        expect(article.errors[:content]).to include("has already been taken")
       end
 
       it "directs to article/index page" do
