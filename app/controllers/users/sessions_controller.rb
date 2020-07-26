380a8d5035f7da5487e3f56bc43436b2b2efcb65
Shogo Hida <shogo.hida@gmail.com>
2020-07-26 17:44:11 +0900

    fixed redirect error to redirect back after test user log in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,8 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path
+    #redirect_to root_path
+    redirect_back(fallback_location: root_path, notice: "Logged in as a test user")
     # add AJAX?, redirect to somewhere else
   end
 end
